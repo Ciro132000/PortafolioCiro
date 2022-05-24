42a5b55ce0606d091a882b87e8d6d3bd8b4d83bb
Ciro132000 <c.g_ciro@hotmail.com>
2022-05-24 18:01:29 -0500

    Estructurando el proyecto

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardComponent } from './components/card/card.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    CardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CardComponent,
+    NavBarComponent
+  ]
+})
+export class SharedModule { }
